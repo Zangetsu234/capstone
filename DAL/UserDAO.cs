cb46f962b18de94568079bf06eac205ef2f489ec
Colton Cantrell <coltonpersonal@gmail.com>
2014-01-10 09:45:26 -0500

    Created User class

--- DAL/UserDAO.cs
+++ DAL/UserDAO.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    class UserDAO
+    public class UserDAO
     {
+        public void Write(string statement, SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=capstone;Integrated Security=SSPI;"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(statement, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parameters);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
