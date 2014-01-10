8f3ed8e6b760a9645b51057c0a0185d6859de00e
Colton Cantrell <coltonpersonal@gmail.com>
2014-01-10 11:14:51 -0500

    Added UserPassFM class to User folder in BLL

---
+++ BLL/User/UserPassFM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class UserPassFM
+    {
+        public int ID { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string VerifyPassword { get; set; }
+        public UserPassFM(User user)
+        {
+            this.CurrentPassword = user.Password;
+            this.NewPassword = user.Password;
+            this.VerifyPassword = user.Password;
+            this.ID = user.ID;
+        }
+        public UserPassFM()
+	    {
+
+	    }
+    }
+}
