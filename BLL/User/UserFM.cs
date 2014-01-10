881ebbc6b0fa94b3ec9baf256a9aa600518a503d
Colton Cantrell <coltonpersonal@gmail.com>
2014-01-10 10:40:04 -0500

    Added references to layers and created UserFM, VM, and Service in folder User in BLL

---
+++ BLL/User/UserFM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class UserFM
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserFM(User user)
+        {
+            this.ID = user.ID;
+            this.Username = user.Username;
+            this.Email = user.Email;
+            this.Password = user.Password;
+        }
+        public UserFM()
+	    {
+
+	    }
+
+    }
+}
