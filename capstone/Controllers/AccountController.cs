263a8b2b09b35e7a97638b6b71085f6d79c0be25
Colton Cantrell <coltonpersonal@gmail.com>
2014-01-10 13:05:42 -0500

    Created AccountController with Login, Logout and Register methods

---
+++ capstone/Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+
+namespace capstone.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["ID"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserLoginFM credientials)
+        {
+            UserLoginVM user = new UserService().UserLogin(credientials);
+            if (user != null)
+            {
+                Session["ID"] = user.ID;
+                Session["Name"] = user.Email;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Username or Password incorrect.";
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session["ID"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(UserRegisterFM registerFM)
+        {
+            UserService users = new UserService();
+            UserLoginFM login = new UserLoginFM();
+            if (registerFM.Email != null && users.ValidEmail(registerFM.Email))
+            {
+                if (users.ConfirmEmailLength(registerFM))
+                {
+                    if (users.IsValidUser(registerFM))
+                    {
+                        if (registerFM.Password != null && registerFM.Password.Length > 7 && registerFM.Password.Length < 26 && registerFM.Password == registerFM.ConfirmPassword)
+                        {
+                            users.CreateUser(registerFM);
+                            login.Email = registerFM.Email;
+                            login.Password = registerFM.Password;
+                            UserLoginVM user = new UserService().UserLogin(login);
+                            Session["ID"] = user.ID;
+                            Session["Name"] = user.Email;
+                            return RedirectToAction("Index", "Home");
+                        }
+                        ViewBag.ErrorMessage = "Passwords must be more than seven characters, less than 25 characters, and match.";
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Email already exists.";
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Invalid email.";
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Email not valid.";
+            }
+            return View();
+        }
+	}
+}
