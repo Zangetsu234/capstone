97dc4f3d4379bdb79ffe12545f1e58272442b917
Colton Cantrell <coltonpersonal@gmail.com>
2014-01-10 11:06:06 -0500

    Created methods for creating email and updating user in UserService

--- BLL/User/UserService.cs
+++ BLL/User/UserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
 using DAL;
 
 namespace BLL
@@ -23,6 +24,68 @@ namespace BLL
                 usersVM.Users.Add(userVM);
             }
             return usersVM;
+        }
+        public bool CreateUser(UserRegisterFM userFM)
+        {
+            if (IsValidUser(userFM))
+            {
+                UserDAO dao = new UserDAO();
+                User user = new User();
+                user.Username = userFM.Username;
+                user.Email = userFM.Email;
+                user.Password = userFM.Password;
+                dao.CreateUser(user);
+                return true;
+            }
+            return false;
+        }
+        public bool IsValidUser(UserRegisterFM userFM)
+        {
+            UserDAO dao = new UserDAO();
+            if (dao.GetUserByEmail(userFM.Email) == null)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool ValidEmail(string email)
+        {
+            if (email.Length < 101)
+            {
+                try
+                {
+                    var addr = new MailAddress(email);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        public bool ConfirmEmailLength(UserRegisterFM userFM)
+        {
+            UserDAO dao = new UserDAO();
+            if (userFM.Email != null && userFM.Email.Length > 5)
+            {
+                return true;
+            }
+            return false;
+        }
+        public UserFM GetUserFM(int ID)
+        {
+            UserDAO dao = new UserDAO();
+            User user = dao.GetUserByID(ID);
+            UserFM userFM = new UserFM(user);
+            return userFM;
+        }
+        public void UpdateUser(UserFM userFM)
+        {
+            UserDAO dao = new UserDAO();
+            User user = dao.GetUserByID(userFM.ID);
+            user.Email = userFM.Email;
+            dao.UpdateUser(user);
         }
 
     }
